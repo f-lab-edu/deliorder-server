58e9af79bbd7e1441e3bc4f2f649c5b6f26b221e
soo <dlgustn96@gmail.com>
2025-11-13 10:29:01 +0900

    chore: @LocalServerPort 제거

--- api/src/test/java/com/deliorder/api/domain/store/api/CategoryControllerTest.java
+++ api/src/test/java/com/deliorder/api/domain/store/api/CategoryControllerTest.java
@@ -19,9 +19,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CategoryControllerTest {
 
-    @LocalServerPort
-    private int port;
-
     @Autowired
     private TestRestTemplate restTemplate;
 
