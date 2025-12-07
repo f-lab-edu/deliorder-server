7fe3714b0f6e8d406b419ff1e99d72bf0bd31091
soo <dlgustn96@gmail.com>
2025-12-08 00:41:44 +0900

    refactor: AWS S3 프로퍼티 값 바인딩 방식 변경(@Value -> @ConfigurationProperties)

--- api/src/main/java/com/deliorder/api/service/S3Service.java
+++ api/src/main/java/com/deliorder/api/service/S3Service.java
@@ -2,11 +2,11 @@ package com.deliorder.api.service;
 
 import com.deliorder.api.api.dto.PresignedUrlRequest;
 import com.deliorder.api.api.dto.PresignedUrlResponse;
+import com.deliorder.api.common.config.S3Properties;
 import com.deliorder.api.common.exception.ErrorCode;
 import com.deliorder.api.common.exception.HandledException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
@@ -24,21 +24,18 @@ import java.util.UUID;
 public class S3Service {
 
     private final S3Presigner s3Presigner;
-
-    @Value("${spring.cloud.aws.s3.bucket}")
-    private String bucketName;
-
-    @Value("${spring.cloud.aws.s3.presigned-url-expiration}")
-    private int expirationMinutes;
+    private final S3Properties s3Properties;
 
     private static final String S3_URL_PREFIX = "https://";
     private static final String S3_URL_SUFFIX = ".s3.amazonaws.com/";
 
     public PresignedUrlResponse createPresignedGetUrl(Long menuId, PresignedUrlRequest request) {
         try {
+            String bucketName = s3Properties.getS3().getBucket();
+            Long presignedUrlExpiration = s3Properties.getS3().getPresignedUrlExpiration();
 
             String fileExtension = extractExtension(request.getFileName());
-            String key = "menu/" + menuId + "/" + UUID.randomUUID() + fileExtension;
+            String key = generateKey(menuId, fileExtension);
 
             PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                     .bucket(bucketName)
@@ -48,13 +45,13 @@ public class S3Service {
 
             PresignedPutObjectRequest presignedRequest = s3Presigner.presignPutObject(
                     PutObjectPresignRequest.builder()
-                            .signatureDuration(Duration.ofMinutes(expirationMinutes))
+                            .signatureDuration(Duration.ofMinutes(presignedUrlExpiration))
                             .putObjectRequest(putObjectRequest)
                             .build()
             );
 
             String presignedUrl = presignedRequest.url().toString();
-            String fileUrl = getFileUrl(key);
+            String fileUrl = getFileUrl(bucketName, key);
 
             return PresignedUrlResponse.builder()
                     .presignedUrl(presignedUrl)
@@ -67,7 +64,11 @@ public class S3Service {
         }
     }
 
-    private String getFileUrl(String key) {
+    private static String generateKey(Long menuId, String fileExtension) {
+        return "menu" + menuId + "/" + UUID.randomUUID() + fileExtension;
+    }
+
+    private String getFileUrl(String bucketName, String key) {
         return S3_URL_PREFIX + bucketName + S3_URL_SUFFIX + key;
     }
 
